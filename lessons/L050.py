30421dd328e0dd897e2e964a9e76276b06468b09
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2024-09-16 00:15:06 +0200

    Errors list : Lessons are the first element of row

--- lessons/L050.py
+++ lessons/L050.py
@@ -1,18 +1,2 @@
-lesson = ['Leçon cinquante',
- 'Faire (faisant) [la] queue',
- "- Felipe, [mon] beau, donne-moi dix tranches de jambon blanc (de York), s'il te plaît.",
- "- Pas question (De ça rien), madame. J'étais [là] avant. 3 Quand je suis arrivé (j'arrivai), j'ai demandé (je demandai) mon tour (la fois) à la fille qui vient de sortir. 4 Et [ensuite] moi je l'ai donné (lui la donnai) à cette dame.",
- "Autrement dit (Ou soit que), maintenant c'est mon tour (me touche à moi), et vous, à (à faire) [la] queue comme (égal que) les autres !",
- "- Je vous en (le) prie, monsieur, c'est que je ne peux pas rester (être) debout très longtemps,",
- "et depuis que je me suis levée (je me levai), je n'ai pas arrêté.",
- "J'ai seulement besoin d'un petit peu de jambon pour mes petits-enfants, les pauvres.",
- '- Bon, passez, mais que [ça] soit rapide.',
- '- Ah, comme (combien) je vous en (le) remercie ! Vous êtes très aimable.',
- 'Voyons, Felipe, mets-moi aussi un quart de jambon cru (montagnard), un demi-kilo de viande de bœuf hachée,',
- 'quatre cuisses de poulet, deux blancs [de volaille] et six côtes de porc.']
-
-exercise1_correction = ["Je t'ai demandé dix tranches de jambon blanc et un demi-kilo de viande hachée.",
- 'Vous êtes très aimable, monsieur, je vous en remercie beaucoup.',
- "Je n'ai pas arrêté de travailler depuis que je me suis levé.",
- "Pas question, madame, j'étais là avant et maintenant c'est mon tour.",
- "J'ai besoin de quatre cuisses de poulet et de deux blancs de volaille."]
+lesson = ['\ufeffLeçon cinquante', 'Faire (faisant) [la] queue', "- Felipe, [mon] beau, donne-moi dix tranches de jambon blanc (de York), s'il te plaît.", "- Pas question (De ça rien), madame. J'étais [là] avant.", "Quand je suis arrivé (j'arrivai), j'ai demandé (je demandai) mon tour (la fois) à la fille qui vient de sortir.", "Et [ensuite] moi je l'ai donné (lui la donnai) à cette dame.", "Autrement dit (Ou soit que), maintenant c'est mon tour (me touche à moi), et vous, à (à faire) [la] queue comme (égal que) les autres !", "- Je vous en (le) prie, monsieur, c'est que je ne peux pas rester (être) debout très longtemps,", "et depuis que je me suis levée (je me levai), je n'ai pas arrêté.", "J'ai seulement besoin d'un petit peu de jambon pour mes petits-enfants, les pauvres.", '- Bon, passez, mais que [ça] soit rapide.', '- Ah, comme (combien) je vous en (le) remercie ! Vous êtes très aimable.', 'Voyons, Felipe, mets-moi aussi un quart de jambon cru (montagnard), un demi-kilo de viande de bœuf hachée,', 'quatre cuisses de poulet, deux blancs [de volaille] et six côtes de porc.'] 
+exercise1_correction = ["Je t'ai demandé dix tranches de jambon blanc et un demi-kilo de viande hachée.", 'Vous êtes très aimable, monsieur, je vous en remercie beaucoup.', "Je n'ai pas arrêté de travailler depuis que je me suis levé.", "Pas question, madame, j'étais là avant et maintenant c'est mon tour.", "J'ai besoin de quatre cuisses de poulet et de deux blancs de volaille."]
