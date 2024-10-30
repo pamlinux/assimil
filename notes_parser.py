9f2363c6e356f2aad8d806005650b538fcf90c9f
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2024-10-30 17:24:47 +0100

    head is now correct in notes_parser

--- notes_parser.py
+++ notes_parser.py
@@ -3,55 +3,55 @@ from html.parser import HTMLParser
 class NotesParser(HTMLParser):
 
     def analyze(self, notes):
-        self._paragraph = ""
         self.notes = []
+        self._buffer = ""
         self.feed(notes)
 
     def handle_starttag(self, tag, attrs):
         if tag == 'html':
             self._section = "html"
         elif tag == 'head':
-            self._head = ""
             self._section = "head"
         elif tag == 'p':
             self._section = "paragraph"
-            self._paragraph = ""
+            self._buffer = ""
             self._note_number_expected = True
         else:
             if self._section == "paragraph":
-                self._paragraph += f'<{tag}'
+                self._buffer += f'<{tag}'
                 for attr in attrs:
-                    self._paragraph += f" {attr[0]}={attr[1]}"
-                self._paragraph += '>'
+                    self._buffer += f""" {attr[0]}="{attr[1]}\""""
+                self._buffer += '>'
             elif self._section == "head":
-                self._head += f'<{tag}'
+                self._buffer += f'<{tag}'
                 for attr in attrs:
-                    self._head += f" {attr[0]}={attr[1]}"
-                self._head += '>'
+                    self._buffer += f""" {attr[0]}="{attr[1]}\""""
+                self._buffer += '>'
 
 
     def handle_endtag(self, tag):
         if tag == 'head':
             self._section = ""
+            self._head = self._buffer
         elif tag == 'p':
             try:
                 note_number = int(self._note_number)
-                self.notes.append((note_number, self._paragraph))
+                self.notes.append((note_number, self._buffer))
                 self._note_number = ''
             except:
                 pass
         else:
-            self._paragraph += f'</{tag}>'
+            self._buffer += f'</{tag}>'
 
     def handle_data(self, data):
         if self._section == 'head':
-            self._head += data
+            self._buffer += data
         elif self._section == "paragraph":
             if self._note_number_expected:
                 self._note_number_expected = False
                 self._note_number = data
             else:
-                self._paragraph += data
+                self._buffer += data
         else:
             print(self._section)
             raise
